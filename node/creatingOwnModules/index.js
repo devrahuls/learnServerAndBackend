32ac6f9f60bc41884295cfefd633f6d0653fec6b
rahull56 <printfrahulsaw@gmail.com>
2023-06-18 19:08:21 +0530

    another way for creating module

--- node/creatingOwnModules/index.js
+++ node/creatingOwnModules/index.js
@@ -1,5 +1,7 @@
 //Everything, and everline in Node is a module itself.
 
-const lib = require('./lib')
+// const lib = require('./lib')
 
-console.log(lib.diff(6, 9) , lib.sum(6, 9));
+import {diff, sum} from './lib.js'
+
+console.log(diff(6, 9) , sum(6, 9));
--- node/creatingOwnModules/lib.js
+++ node/creatingOwnModules/lib.js
@@ -1,7 +1,20 @@
-exports.diff = (a,b) => {
+// exports.diff = (a,b) => {
+//     return a - b;
+// }
+
+// exports.sum = (a,b) =>{
+//     return a + b;
+// }
+
+//OR
+
+
+const diff = (a,b) => {
     return a - b;
 }
 
-exports.sum = (a,b) =>{
+const sum = (a,b) =>{
     return a + b;
 }
+
+export {diff, sum}
